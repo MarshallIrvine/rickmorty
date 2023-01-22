7587a668c0e8734eabd80745257728328cba8604
DrIrvine <72073872+MarshallIrvine@users.noreply.github.com>
2023-01-22 16:00:39 +0100

    add character api data classes

---
+++ app/src/main/java/com/marshall/irvine/rickmorty/data/model/Info.kt
@@ -0,0 +1,8 @@
+package com.marshall.irvine.rickmorty.data.model
+
+data class Info(
+    val count: Int,
+    val next: String,
+    val pages: Int,
+    val prev: Any
+)
---
+++ app/src/main/java/com/marshall/irvine/rickmorty/data/model/Location.kt
@@ -0,0 +1,6 @@
+package com.marshall.irvine.rickmorty.data.model
+
+data class Location(
+    val name: String,
+    val url: String
+)
